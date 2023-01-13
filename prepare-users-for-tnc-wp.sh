9d9c4cec336c5e8ddad031555aa75c71a44d448e
Luke S Thompson <73861168+lsthompson@users.noreply.github.com>
2023-01-13 19:26:04 +1100

    Adjust perms; change token name to be per-user

--- prepare-users-for-tnc-wp.sh
+++ prepare-users-for-tnc-wp.sh
@@ -7,7 +7,7 @@ for user in `\ls -A1 /var/cpanel/users/`
 do
   mkdir -p /home/${user}/.tnc/
   echo ${user} > /home/${user}/.tnc/cp-username
-  uapi --output=jsonpretty --user=${user} Tokens create_full_access name='TNC-WP-TOOLBOX' | jq -r '.result.data.token' > /home/${user}/.tnc/cp-api-key
+  uapi --output=jsonpretty --user=${user} Tokens create_full_access name='TNC-WP-Toolbox-${user}' | jq -r '.result.data.token' > /home/${user}/.tnc/cp-api-key
   chown ${user}:${user} -R /home/${user}/.tnc/
-  chmod 0400 /home/${user}/.tnc/cp-*
+  chmod 0600 /home/${user}/.tnc/cp-*
 done
