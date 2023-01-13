e35a374b32aa1600b7c4fcae8f7418f32f41bbb9
Luke S Thompson <73861168+lsthompson@users.noreply.github.com>
2023-01-13 17:09:35 +1100

    Update function to automatically find user/host; and pull in API key

--- php-nginx-cache-clear-function.php
+++ php-nginx-cache-clear-function.php
@@ -1,12 +1,12 @@
 <?php
 
 function clear_nginx_cache() {
-    // Get the cPanel API token
-    $api_token = 'YOUR_API_TOKEN_HERE';
-    // Get the server hostname
-    $server_hostname = 'YOUR_SERVER_HOSTNAME_HERE';
     // Get the cPanel username
-    $cpanel_username = 'YOUR_CPANEL_USERNAME_HERE';
+    $cpanel_username = get_current_user();
+    // Get the API token
+    $api_token = file_get_contents("/home/".$cpanel_username."/.tnc/cp-api-key");
+    // Get the server hostname
+    $server_hostname = gethostname();
     // Build the headers for the request
     $headers = array(
         'Authorization' => 'cpanel '. $cpanel_username . ':' . $api_token,
@@ -16,7 +16,7 @@ function clear_nginx_cache() {
         'parameter' => 'value',
     );
     // Build the URL for the request
-    $url = 'https://' . $server_hostname . ':2083/execute/Module/function';
+    $url = 'https://' . $server_hostname . ':2083/execute/NginxCaching/clear_cache';
     // Make the request
     $response = wp_remote_post( $url, array(
         'headers' => $headers,
@@ -26,14 +26,15 @@ function clear_nginx_cache() {
     if ( ! is_wp_error( $response ) && $response['response']['code'] === 200 ) {
         // Display a message to the user
         echo '<div class="notice notice-success is-dismissible">';
-        echo '<p>Cache cleared successfully!</p>';
+        echo '<p>NGINX Cache cleared successfully!</p>';
         echo '</div>';
     } else {
         // Display an error message to the user
         echo '<div class="notice notice-error is-dismissible">';
-        echo '<p>An error occurred while trying to clear the cache. Please try again later.</p>';
+        echo '<p>An error occurred while trying to clear the NGINX Cache. Please contact us if this persists in 60 minutes.</p>';
         echo '</div>';
     }
 }
+
 
 ?>
