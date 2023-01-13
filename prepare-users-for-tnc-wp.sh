f2f117f6af699253da9238721852492f71c4c002
Luke S Thompson <73861168+lsthompson@users.noreply.github.com>
2023-01-13 23:26:13 +1100

    Add root check; jq check; tidy-up comments & loop

--- prepare-users-for-tnc-wp.sh
+++ prepare-users-for-tnc-wp.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Creates necessary folder/s and file/s to enable usage of the TNC WP Toolbox plugin.
-# Will create an unrestricted cPanel API Token and store this for the plugin to auth.
+# Will create unrestricted cPanel API Tokens & store them for each install to use.
+# The Network Crew Pty Ltd :: https://github.com/LEOPARD-host/TNC-WP-Toolbox/
 
+# Require root to run
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Check for jq, install if not present
+rpm -qa | grep -qw jq || yum -y install jq
+
+# Loop through users to make dir/file, create API Token
 for user in `\ls -A1 /var/cpanel/users/`
 do
   mkdir -p /home/${user}/.tnc/
   uapi --output=jsonpretty --user=${user} Tokens create_full_access name='TNC-WP-TOOLBOX' | jq -r '.result.data.token' > /home/${user}/.tnc/cp-api-key
   chown ${user}:${user} -R /home/${user}/.tnc/
-  chmod 0600 /home/${user}/.tnc/cp-*
+  chmod 0600 /home/${user}/.tnc/cp-api-key
 done
