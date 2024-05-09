997e6efa0baa52680f4cf48c881fb24bb4fd827a
Luke S Thompson <73861168+lsthompson@users.noreply.github.com>
2024-05-09 20:06:26 +1000

    Fix tiny tabbing issue

--- tnc-toolbox/core/class-tnc-wp-toolbox.php
+++ tnc-toolbox/core/class-tnc-wp-toolbox.php
@@ -100,7 +100,7 @@ if ( ! class_exists( 'Tnc_Wp_Toolbox' ) ) :
 		 */
 		public static function instance() {
 			if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Tnc_Wp_Toolbox ) ) {
-				self::$instance					= new Tnc_Wp_Toolbox;
+				self::$instance				= new Tnc_Wp_Toolbox;
 				self::$instance->includes();
 				self::$instance->helpers		= new Tnc_Wp_Toolbox_Helpers();
 				self::$instance->settings		= new Tnc_Wp_Toolbox_Settings();
