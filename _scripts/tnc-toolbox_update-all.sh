2a3e5d86e876a3672c421542d00f228dd91473e9
Luke S Thompson <73861168+lsthompson@users.noreply.github.com>
2025-11-17 17:36:16 +1100

    Lint, what lint?

--- _scripts/tnc-toolbox_update-all.sh
+++ _scripts/tnc-toolbox_update-all.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-/*
-    TNC Toolbox: Web Performance (for WordPress)
-    
-    Copyright (C) The Network Crew Pty Ltd (TNC)
-    PO Box 3113 Uki 2484 NSW Australia https://tnc.works
-
-    https://github.com/The-Network-Crew/TNC-Toolbox-for-WordPress
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+#    TNC Toolbox: Web Performance (for WordPress)
+#    
+#    Copyright (C) The Network Crew Pty Ltd (TNC)
+#    PO Box 3113 Uki 2484 NSW Australia https://tnc.works
+#
+#    https://github.com/The-Network-Crew/TNC-Toolbox-for-WordPress
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # NOTE: This script assumes a properly functional CageFS set-up on your server.
 # CageFS: If you do not have this set-up, change/comment "wp" & install separately.
